c6a1bb5faab52cd306adc5098b0767282c9cf801
정영호 <youngh0910@gmail.com>
2022-04-16 13:07:39 +0900

    Refact : 핸드폰 인증 validate 핸드폰 번호 추가

--- src/main/java/app/project/ably/auth/dto/PersonalIdentificationCheckDTO.java
+++ src/main/java/app/project/ably/auth/dto/PersonalIdentificationCheckDTO.java
@@ -6,13 +6,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 @Data
 @Builder @AllArgsConstructor @NoArgsConstructor
 public class PersonalIdentificationCheckDTO {
 
-    @ApiModelProperty(value = "verificationCode", required = true)
+    @ApiModelProperty(value = "verificationCode", example = "1234", required = true)
     private String verificationCode;
 
-    @ApiModelProperty(value = "phoneNumber", required = true)
+    @ApiModelProperty(value = "phoneNumber", example = "010-1234-5678", required = true)
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "{exception.validate.phoneNumber}")
+    @NotEmpty
     private String phoneNumber;
 }
--- src/main/java/app/project/ably/auth/dto/PersonalIdentificationSendDTO.java
+++ src/main/java/app/project/ably/auth/dto/PersonalIdentificationSendDTO.java
@@ -6,10 +6,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 @Data
 @Builder @AllArgsConstructor @NoArgsConstructor
 public class PersonalIdentificationSendDTO {
 
-    @ApiModelProperty(value = "phoneNumber", required = true)
+    @ApiModelProperty(value = "phoneNumber", example = "010-1234-5678", required = true)
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "{exception.validate.phoneNumber}")
+    @NotEmpty
     private String phoneNumber;
 }
